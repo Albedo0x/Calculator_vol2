c661164fa41105d8e895c35f845985d731278581
Albedo0x <nikita.v.katyshev@gmail.com>
2022-11-25 09:59:02 +0300

    New file with Calculator class

---
+++ calc.mjs
@@ -0,0 +1,51 @@
+// Описание основного класса
+
+export class Calculator {
+  constructor(options) {
+    this.$screen = document.getElementById(options.selector);
+    this.number1 = options.number1;
+    this.number2 = options.number2;
+    this.operation = options.operation;
+    this.state = options.state;
+    this.maxN = options.maxN;
+  }
+
+  show(info) {
+    this.$screen.innerHTML = info;
+  }
+
+  onLoadClearing() {
+    this.$screen.innerHTML = "0";
+    this.number1 = "0";
+    this.number2 = "";
+    this.operation = "";
+    this.state = false;
+  }
+
+  onCalcClearing(info) {
+    this.show(info);
+    this.state = true;
+    this.number2 = "";
+    this.operation = "";
+  }
+
+  calculate(operation) {
+    switch (operation) {
+      case "+":
+        this.number1 = +(+this.number1 + +this.number2).toFixed(3);
+        return this.number1;
+      case "-":
+        this.number1 = +(+this.number1 - +this.number2).toFixed(3);
+        return this.number1;
+      case "*":
+        this.number1 = +(+this.number1 * +this.number2).toFixed(3);
+        return this.number1;
+      case "÷":
+        this.number1 = +(+this.number1 / +this.number2).toFixed(3);
+        return this.number1;
+      case "^":
+        this.number1 = +((+this.number1) ** +this.number2).toFixed(3);
+        return this.number1;
+    }
+  }
+}
--- index.mjs
+++ index.mjs
@@ -1,140 +1,102 @@
-//Объявление переменных и констант
+import { Calculator } from "./calc.mjs";
 
-const calcInput = document.getElementById("main-input");
-const inputScreen = document.getElementById("screen");
-const maxN = 999999999999;
-let number1 = "";
-let number2 = "";
-let operation = "";
-let state = false;
-let info = "";
-let calculator;
-
-class Calculator {
-  constructor(number1, number2, operation) {
-    this.number1 = number1;
-    this.number2 = number2;
-    this.operation = operation;
-  }
-
-  show(info) {
-    inputScreen.innerHTML = info;
-  }
-
-  clear() {
-    inputScreen.innerHTML = "0";
-  }
-
-  onLoadClearing() {
-    this.clear();
-    number1 = "";
-    number2 = "";
-    operation = "";
-  }
-
-  onCalcClearing(info) {
-    this.show(info);
-    state = true;
-    number2 = "";
-    operation = "";
-  }
-
-  calculate(number1, number2, operation) {
-    switch (operation) {
-      case "+":
-        number1 = +(+number1 + +number2).toFixed(3);
-        return number1;
-      case "-":
-        number1 = +(+number1 - +number2).toFixed(3);
-        return number1;
-      case "*":
-        number1 = +(+number1 * +number2).toFixed(3);
-        return number1;
-      case "÷":
-        number1 = +(+number1 / +number2).toFixed(3);
-        return number1;
-      case "^":
-        number1 = +((+number1) ** +number2).toFixed(3);
-        return number1;
-    }
-  }
-}
-calculator = new Calculator(number1, number2, operation);
+let calculator = new Calculator({
+  selector: "screen",
+  number1: "",
+  number2: "",
+  operation: "",
+  state: false,
+  maxN: 999999999999,
+});
 
 calculator.onLoadClearing();
 
-calcInput.addEventListener("click", (event) => {
+document.getElementById("main-input").addEventListener("click", (event) => {
   let clickedBtn = event.target;
 
   // Если нажал не на кнопку - ничего не делаем
-
   if (!clickedBtn.classList.contains("calculator-btn")) {
     return;
   }
-
   // Если нажал на "C" - очистить экран и переменные
-
   if (clickedBtn.classList.contains("btn-clear-all")) {
     calculator.onLoadClearing();
-    console.log(number1, operation, number2, state);
+  }
+  // Если нажал на "MR" - сохранить значение
+
+  if (clickedBtn.classList.contains("btn-save")) {
+    localStorage.setItem("savedNumber", calculator.number1);
   }
 
+  // Если нажал на "MC" - взять сохранённое значениеw
+
+  if (clickedBtn.classList.contains("btn-pull")) {
+    let pullItem = localStorage.getItem("savedNumber");
+    if (calculator.operation === "" && calculator.number2 === "") {
+      calculator.show(pullItem);
+      calculator.number1 = pullItem;
+    }
+    if (calculator.operation != "") {
+      calculator.show(pullItem);
+      calculator.number2 = pullItem;
+    }
+  }
+  // Если нажал на "=" - выполнить рассчёт
   if (clickedBtn.classList.contains("btn-result")) {
-    if (number2 === "" && operation != "") {
-      number1 = calculator.calculate(number1, number1, operation);
-      console.log(number1, operation, number2, state);
-      number1 > maxN
-        ? (inputScreen.innerHTML = "too Much")
-        : calculator.onCalcClearing(number1);
+    if (calculator.number2 === "" && calculator.operation != "") {
+      calculator.number1 = calculator.calculate(calculator.operation);
+      calculator.number1 > calculator.maxN
+        ? calculator.show("Too Much")
+        : calculator.onCalcClearing(calculator.number1);
     }
-    if (number2 === "" && operation === "") {
-      console.log(number1, operation, number2, state);
-      calculator.onCalcClearing(number1);
+    if (calculator.number2 === "" && calculator.operation === "") {
+      calculator.onCalcClearing(calculator.number1);
     } else {
-      console.log(number1, operation, number2, state);
-      number1 = calculator.calculate(number1, number2, operation);
-      console.log(number1, operation, number2, state);
-      calculator.onCalcClearing(number1);
-      console.log(number1, operation, number2, state);
+      calculator.number1 = calculator.calculate(calculator.operation);
+      calculator.number1 > calculator.maxN
+        ? calculator.show("Too Much")
+        : calculator.onCalcClearing(calculator.number1);
     }
   }
-
+  // Если нажал на знак операции "+,-,/,^,*" - выбрать операцию
   if (clickedBtn.classList.contains("btn-operation")) {
-    if (number1 !== "" && state === true) {
-      state = false;
-      operation = clickedBtn.innerHTML;
-      calculator.show(operation);
-      console.log(number1, operation, number2, state);
+    if (calculator.number1 !== "" && calculator.state === true) {
+      calculator.state = false;
+      calculator.operation = clickedBtn.innerHTML;
+      calculator.show(calculator.operation);
     } else {
-      operation = clickedBtn.innerHTML;
-      calculator.show(operation);
-      console.log(number1, operation, number2, state);
+      calculator.operation = clickedBtn.innerHTML;
+      calculator.show(calculator.operation);
     }
   }
-
+  // Если нажал на цифру - начать набирать число
   if (clickedBtn.classList.contains("btn-digit")) {
-    if (state === false) {
-      if (operation === "" && number2 === "" && number1 === "0") {
-        number1 = clickedBtn.innerHTML;
-        calculator.show(number1);
-        console.log(number1, operation, number2, state);
+    if (calculator.state === false) {
+      if (
+        calculator.operation === "" &&
+        calculator.number2 === "" &&
+        calculator.number1 === "0"
+      ) {
+        calculator.number1 = clickedBtn.innerHTML;
+        calculator.show(calculator.number1);
         return;
       }
-      if (operation === "" && number2 === "" && number1 != "0") {
-        number1 = number1 + clickedBtn.innerHTML;
-        calculator.show(number1);
-        console.log(number1, operation, number2, state);
+      if (
+        calculator.operation === "" &&
+        calculator.number2 === "" &&
+        calculator.number1 != "0"
+      ) {
+        calculator.number1 = calculator.number1 + clickedBtn.innerHTML;
+        calculator.show(calculator.number1);
         return;
       } else {
-        number2 = number2 + clickedBtn.innerHTML;
-        calculator.show(number2);
-        console.log(number1, operation, number2, state);
+        calculator.number2 = calculator.number2 + clickedBtn.innerHTML;
+        calculator.show(calculator.number2);
       }
     } else {
-      number1 = clickedBtn.innerHTML;
-      calculator.show(number1);
-      state = false;
-      console.log(number1, operation, number2, state);
+      calculator.number1 = clickedBtn.innerHTML;
+      calculator.show(calculator.number1);
+      calculator.state = false;
     }
   }
 });
